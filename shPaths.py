e2f83f02c14dd89e24ee705c9d987ebb01597b5d
dmartin <david@dadahasa.com>
2017-12-02 19:24:43 -0600

    Add QA Paths to test BOX repository in a local folder

--- shPaths.py
+++ shPaths.py
@@ -8,6 +8,41 @@ csvZipFileName = '_CSVReports.zip'
 #---------------------------------------------------
 ###<------------- PRODUCTION
 #---------------------------------------------------
+def qa_path():
+    ###QA Testing
+    siteEnv = 'QA'
+    #REMOTE STORAGE
+    ##'Z: F:\Users\David\'
+    # If the repository is on the same server as the application
+    #(instead of in a network share) it will result in a "permision denied"
+    #when trying to save the slide decks. Not sure yet why.
+    drive = 'F:/Users/David' #manually mapped on Windows to the parent of 'startFolder'
+    startFolder = '/Repository/'
+    shFolder = '/' # SH reports are placed directly on the custmer's folder.
+    #LOCAL SERVER
+    localWorkFolder = 'F:/Users/David/Desktop/SH-COLLECTOR/'
+    # Slide Deck Template
+    slides_template_path = localWorkFolder + 'shTemplate.pptx'
+    #log files
+    csvLogFile = localWorkFolder + 'shLog.csv'
+    tmpLogFile = localWorkFolder + 'shLogTemp.csv'
+    #----common
+    collectorFolder =  localWorkFolder + 'Downloads/'
+    csvTempFolder = collectorFolder + 'csvTemp/'
+    archiveFolder = collectorFolder + 'ARCHIVE/'
+
+    qa_paths = (
+        siteEnv,
+        drive, startFolder, shFolder, 
+        localWorkFolder, 
+        slides_template_path,
+        csvLogFile, tmpLogFile,
+        collectorFolder, csvTempFolder, archiveFolder)
+        
+    return qa_paths
+#---------------------------------------------------
+###<------------- PRODUCTION
+#---------------------------------------------------
 def prod_path():
     ###PRODUCTION
     siteEnv = 'PROD'
@@ -40,7 +75,7 @@ def prod_path():
 #---------------------------------------------------
 #<------------- LAB
 #---------------------------------------------------
-def lab_paths():
+def lab_path():
     ###LAB
     siteEnv = 'LAB'
     #REMOTE STORAGE
@@ -72,7 +107,7 @@ def lab_paths():
 #---------------------------------------------------
 #---------------------------------------------------
 
-sitePaths = (lab_paths(), prod_path())
+sitePaths = (qa_path(), lab_path(), prod_path())
 
 #---------------------------------------------------
 #---------------------------------------------------
